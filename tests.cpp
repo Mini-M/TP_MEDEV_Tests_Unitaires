8e31494fcecf6d1a7103b5b6ff597fa5e6e02e92
Irshad Abibouraguimane <irshad.abib@gmail.com>
2015-12-10 19:20:35 +0100

    Ajout de tests unitaires

--- tests.cpp
+++ tests.cpp
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+/*
 int randomPlateauGen (int nbPierres,int t,Goban* jeu) {
     if (2*nbPierres<t*t)
     {
@@ -41,34 +41,62 @@ int randomPlateauGen (int nbPierres,int t,Goban* jeu) {
     }
     return 0;
 }
-
-
+*/
+/* Premières séries de tests : libertes basiques*/
 
 class gobanTest1 : public ::testing::Test {
 protected :
 Goban* jeu;
-Pierre* maPierre;
-vector<coord> libertes;
+Pierre* maPierre_centre,*maPierre_bord,*maPierre_coin;
+int taille_centre, taille_bord, taille_coin;
 virtual void SetUp () {
     jeu = new Goban();
-    //jeu->MiseAJour(new Pierre (jeu,0,2,2));
-    maPierre = new Pierre (jeu,0,2,2);
-    libertes = maPierre->getGroupe()->getlibertes();
+
+    maPierre_centre = new Pierre (jeu,0,2,2);
+    maPierre_bord = new Pierre (jeu,0,0,3);
+    maPierre_coin = new Pierre (jeu,0,4,4);
+
+    taille_centre = maPierre_centre->getGroupe()->getlibertes().size();
+    taille_bord = maPierre_bord->getGroupe()->getlibertes().size();
+    taille_coin = maPierre_coin->getGroupe()->getlibertes().size();
+
+
 }
 
 
 };
 
 
+Pierre* maPierre_centre,*maPierre_bord,*maPierre_coin;
+int taille_centre, taille_bord, taille_coin;
+
+/*Une pierre posee au centre a 4 libertes*/
 TEST_F(gobanTest1, TestLibertes1) {
 
-    EXPECT_EQ (4,libertes.size());
+    EXPECT_EQ (4,taille_centre);
+
+}
+
+/*Une pierre posee au bord a 3 libertes*/
+
+TEST_F(gobanTest1, TestLibertes2) {
+
+    EXPECT_EQ (3,taille_bord);
+
+}
+
+/*Une pierre posee dans le coin a 2 libertes*/
+TEST_F(gobanTest1, TestLibertes3) {
+
+    EXPECT_EQ (2,taille_coin);
 
 }
 
 
 
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+
+
+::testing::InitGoogleTest(&argc, argv);
+return RUN_ALL_TESTS();
 }
